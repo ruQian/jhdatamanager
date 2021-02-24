17b68a6d12b68fd333fd9c3fbd7402b40a347202
rubo <rubo@e-u.cn>
2021-02-24 10:52:57 +0800

    提交代码

--- src/components/Mapper.vue
+++ src/components/Mapper.vue
@@ -33,8 +33,12 @@
   //import {toStringHDMS} from 'ol/coordinate';
   import {boundingExtent} from 'ol/extent';
   import {transformExtent} from 'ol/proj';
-
-
+  import TileImage from 'ol/source/TileImage';
+  import TileGrid from 'ol/tilegrid/TileGrid';
+  import BingMaps from 'ol/source/BingMaps';
+  import Projection from 'ol/proj/Projection';
+  import {addProjection, addCoordinateTransforms} from 'ol/proj';
+  import projzh from 'projzh';
   export default {
     name: 'Mapper',
     data() {
@@ -174,16 +178,81 @@
               return RotateNorthControl;
           }(Control));
 
+          //baidu map       
+                  /**
+         *  定义百度投影，这是实现无偏移加载百度地图离线瓦片核心所在。
+         *  网上很多相关资料在用OpenLayers加载百度地图离线瓦片时都认为投影就是EPSG:3857(也就是Web墨卡托投影)。
+         *  事实上这是错误的，因此无法做到无偏移加载。
+         *  百度地图有自己独特的投影体系，必须在OpenLayers中自定义百度投影，才能实现无偏移加载。
+         *  百度投影实现的核心文件为bd09.js，在迈高图官网可以找到查看这个文件。
+         */
+        const projBD09 = new Projection({
+          // baidu或者BD:09
+          code: 'BD:09',
+          extent: [-20037726.37, -11708041.66, 20037726.37, 12474104.17],
+          units: 'm',
+          axisOrientation: 'neu',
+          global: false
+          });
+          addProjection(projBD09);
+          addCoordinateTransforms('EPSG:4326', projBD09, projzh.ll2bmerc, projzh.bmerc2ll);
+          addCoordinateTransforms('EPSG:3857', projBD09, projzh.smerc2bmerc, projzh.bmerc2smerc);
+
+          // 自定义分辨率和瓦片坐标系
+          const rlProject = [];
+
+          // 计算百度使用的分辨率
+          for (let i = 0; i <= 18; i++) {
+              rlProject[i] = Math.pow(2, 18 - i);
+          }
+          const tilegrid = new TileGrid({
+              origin: [0, 0],    // 设置原点坐标
+              resolutions: rlProject // 设置分辨率
+          });
+
+          /**
+          * 加载百度地图离线瓦片不能用ol.source.XYZ，ol.source.XYZ针对谷歌地图（注意：是谷歌地图）而设计，
+          * 而百度地图与谷歌地图使用了不同的投影、分辨率和瓦片网格。
+          * 因此这里使用ol.source.TileImage来自行指定投影、分辨率、瓦片网格。
+          */
+          const baiduSource = new TileImage({
+              projection: 'BD:09',
+              tileGrid: tilegrid,
+              tilePixelRatio: 2,
+              tileUrlFunction: (tileCoord, pixelRatio, proj) => {
+                  if (!tileCoord) {
+                      return;
+                  }
+                  const z = tileCoord[0];
+                  let x = tileCoord[1];
+                  // 注意，在openlayer3中由于载地图的方式是右上递增
+                  // let y: number | string = tileCoord[2];
+                  // 而openlayer6中是右下递增，所以y的值需要注意
+                  // 6版本需要取负值,同时注意要减一，否则缩放有问题
+                  let y  = -tileCoord[2] - 1;
+
+                  // 百度瓦片服务url将负数使用M前缀来标识
+                  if (x < 0) {
+                      x = 'M' + (-x);
+                  }
+                  if (y < 0) {
+                      y = 'M' + (-y);
+                  }
+                  // online3的3是用来分流的，还可以是其他路径
+                  // udt应该表示的是地图发布日期。p表示地图上的信息，scaler表示缩放
+                  // 0表示不显示地名等标注信息，只单纯的地图地图，1表示显示地名信息
+                  return `http://online3.map.bdimg.com/onlinelabel/?qt=tile&x=${x}&y=${y}&z=${z}&styles=pl&udt=20190426&scaler=2&p=1`;
+              }
+          });
+          //baidumapend
           this.map = new Map({
             target: target,
             controls: defaultControls().extend([new RotateNorthControl()]),
-            layers: [
-              new TileLayer({
-                source: new XYZ({
-                  url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-                })
-              })],
+            layers: [new TileLayer({
+              source: baiduSource,
+                   })],
             view: new View({
+              projection: "EPSG:3857",
               center: fromLonLat([103, 34]),
               zoom: 2,
               extent: transformExtent(extent, 'EPSG:4326', 'EPSG:3857')
